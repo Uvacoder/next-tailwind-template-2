3024182cd176fde6e10d8f1de29c372edb678c30
Narendra Syahrasyad <nanda.s@hey.com>
2021-05-07 10:51:30 -0700

    feat: add mdx bundler

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import React from 'react'
 import Head from 'next/head'
 import 'twin.macro'
 
-export default function Home() {
+import * as fs from 'fs/promises'
+import * as path from 'path'
+import { bundleMDX } from 'mdx-bundler'
+import { getMDXComponent } from 'mdx-bundler/client'
+
+export default function Home({ content, frontmatter }) {
+  const Component = React.useMemo(() => getMDXComponent(content), [content])
   return (
     <>
       <Head>
@@ -9,8 +16,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main tw="min-h-screen flex items-center justify-center">
-        <h1>Hello world!</h1>
+        <h1>{frontmatter.title}</h1>
+        <Component />
       </main>
     </>
   )
+}
+
+export async function getStaticProps() {
+  const mdxSource = await fs.readFile('content/index.mdx')
+  const { code: content, frontmatter } = await bundleMDX(mdxSource, {
+    cwd: path.join(process.cwd(), 'content'),
+  })
+  return {
+    props: {
+      content,
+      frontmatter,
+    },
+  }
 }
